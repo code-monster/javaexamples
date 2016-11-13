ed07f259757ec989202c37877c1fd64ab2b5b912
code-monster <ua-lex@yandex.ru>
2016-11-13 16:16:07 +0200

    accordion task init

---
+++ Accordion/src/com/blogspot/iserf/accordion/Lab_Thread_Thread_Play_The_Accordion.java
@@ -0,0 +1,24 @@
+
+package com.blogspot.iserf.accordion;
+
+
+public class Lab_Thread_Thread_Play_The_Accordion {
+    public static void main(String[] args) throws InterruptedException {
+        for (int k = 0; k < 10; k++) {
+            // A + B
+            Runnable printerA = new PrintRunnable("A   .", 100);
+            Thread threadA = new Thread(printerA);
+            threadA.start();
+            Runnable printerB = new PrintRunnable(".   B", 99);
+            Thread threadB = new Thread(printerB);
+            threadB.start();
+            threadA.join();
+            threadB.join();
+            // C
+            System.out.println("-----");
+            Runnable printerC = new PrintRunnable("  C", 100);
+            printerC.run();
+            System.out.println("-----");
+        }
+    }
+}
